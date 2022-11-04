7cc8d1bf30ff89fb86fd82765b544d3e67c18709
Jonathan Kuhn <jkuhn96@gmail.com>
2022-11-04 05:10:59 -0400

    Follow up for server create feature

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+require('./config/mongoose.config')
 require('./routes/product.routes')(app)
 
 app.listen(port, () => console.log(`Listening on port: ${port}`))
